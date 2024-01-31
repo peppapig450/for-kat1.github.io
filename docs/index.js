24239a917719eebff71e111d67e5f27c6a653d44
peppapig450 <nbgenius1@gmail.com>
2024-01-30 21:18:32 -0800

    bru

--- docs/index.js
+++ docs/index.js
@@ -1,4 +1,6 @@
-document.getElementById('hello-kitty').style.visibility = 'hidden';
+window.addEventListener('DOMContentLoaded', () => {
+    document.getElementById('hello-kitty').style.visibility = 'hidden';
+});
 
 function showHint() {
     window.alert("What is the day we last saw each other?");
@@ -7,11 +9,12 @@ function showHint() {
 function unHide() {
     document.getElementById('hello-kitty').style.visibility = 'visible';
 }
+
 function checkPassword() {
     const possiblePasses = ["January 14", "January 15", "1/14", "1/15"];
 
     var passwordInput = document.getElementById("password").value;
-    
+
     if (possiblePasses.includes(passwordInput)) {
         document.getElementById("password-container").classList.add("hidden");
         unHide();
